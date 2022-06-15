1878ca7dfecccdd625748bce8a92a99174a4b804
Carlos <carlosportella16@hotmail.com>
2022-06-14 21:01:42 -0300

    dto, resources and services

---
+++ backend/src/main/java/com/devsuperior/activity/resources/ClientResource.java
@@ -0,0 +1,10 @@
+package com.devsuperior.activity.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+}
---
+++ backend/src/main/java/com/devsuperior/activity/services/ClientService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.activity.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.activity.dto.ClientDTO;
+import com.devsuperior.activity.entities.Client;
+import com.devsuperior.activity.repositories.ClientRepository;
+import com.devsuperior.activity.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+	
+}
