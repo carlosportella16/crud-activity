6aa6cd34196edf5414aa34c1e220a576af2d99f8
Carlos <carlosportella16@hotmail.com>
2022-06-14 21:36:14 -0300

    Client insert and find by id

--- backend/src/main/java/com/devsuperior/activity/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/activity/services/ClientService.java
@@ -23,5 +23,22 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);		
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
 	
 }
